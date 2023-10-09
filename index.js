6bb93ee8d0820fc8d955351dcc3837e47697593e
Pablo Matos Lima <78696750+pablomtlima@users.noreply.github.com>
2023-10-08 21:08:13 -0300

    d

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
-// external modules
-const inquirer = require('inquirer')
-const chalk = require('chalk')
+import inquirer from 'inquirer';
+import chalk from 'chalk'
 
-// internal modules
-const fs = require('fs')
+import fs from 'fs'
+
+operation()
+
+function operation() {
+    console.clear()
+    inquirer.prompt({
+        type: 'list',
+        name: 'action',
+        message: 'O que você deseja fazer?',
+        choices: [
+            'Criar conta', 'Consultar saldo', 'Depositar', 'Sacar', 'Sair'
+        ],
+    }).then((answer) => {
+        const action = answer['action']
+
+        if (action === 'Criar conta') {
+            createAccount()
+        }
+    }).catch(error => console.log(error))
+}
+
+function createAccount() {
+    console.log(chalk.bgGreen.blk(' Parabéns por escolher o nosso banco '))
+    console.log(chalk.green('Defina as opções da sua conta a seguir'))
+}
