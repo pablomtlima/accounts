70e917b3a42571a941d89c1fd547dd94d76ef5f9
Pablo Matos Lima <78696750+pablomtlima@users.noreply.github.com>
2023-10-08 22:19:52 -0300

    ok

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import inquirer from 'inquirer';
 import chalk from 'chalk'
 
 import fs from 'fs'
+import { error } from 'console';
 
 operation()
 
@@ -26,4 +27,15 @@ function operation() {
 function createAccount() {
     console.log(chalk.bgGreen.blk(' Parabéns por escolher o nosso banco '))
     console.log(chalk.green('Defina as opções da sua conta a seguir'))
+}
+
+function buildAccount() {
+    inquirer.prompt({
+        name: 'accountName',
+        message: 'Digite um nome para a sua conta:'
+    }).then(answer => {
+        console.log(answer)
+    }).catch((error => {
+        console.log(error)
+    }))
 }
